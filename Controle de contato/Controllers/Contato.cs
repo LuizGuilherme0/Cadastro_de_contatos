38a4d1d11f0a4211c5b1c864448a46a32b86585e
Luiz Guilherme Muniz barreto <luiz.barreto@aluno.senai.br>
2023-12-18 19:32:54 -0300

    att

---
+++ Controle de contato/Controllers/Contato.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Controle_de_contato.Controllers
+{
+    public class Contato : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
